24237146ca57418567003461de25e48e55188cc5
Andrea Hernandez <andreajhl29@gmail.com>
2023-09-09 10:29:51 -0300

    feat: :sparkles: add constants

---
+++ src/constants/cart/index.js
@@ -0,0 +1,6 @@
+export const LOAD_CART_FROM_STORAGE = '[Cart] - LoadCart from storage';
+export const UPDATE_PRODUCTS_IN_CART = '[Cart] - Update products in cart';
+export const REMOVE_PRODUCT_IN_CART = '[Cart] - Remove product in cart';
+
+export const CART_KEY = 'cart';
+export const MAX_QUANTITY_ITEMS = 6;
