dcd372f2cc9ebd61c3301b5f39b230b7171d31a9
Andrea Hernandez <andreajhl29@gmail.com>
2023-09-10 16:33:43 -0300

    add wordings

---
+++ src/wordings/index.js
@@ -0,0 +1,25 @@
+export default {
+  navbar: {
+    brandName: 'World Of Shopping',
+    cart: 'My Cart',
+  },
+  cart: {
+    addButton: 'Add to cart',
+    total: 'Total',
+    finishOrder: 'Finish order'
+  },
+  product: {
+    quantity: 'Quantity:',
+    price: 'Price:',
+    addButtom: 'Add',
+    description: 'Description',
+  },
+  warnings: {
+    loading: 'Loading',
+    pageNotFound: {
+      title: 'Oops!',
+      subtitle:"Sorry. We can't find the page",
+      retry: 'Return to Start',
+    },
+  },
+};
